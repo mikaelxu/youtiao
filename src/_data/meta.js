96d566410105a621c276cb803b8178bc40d70edc
mikaelxu <michael.xu@semipin.net>
2024-08-13 22:36:04 +0900

    Update meta.js

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,8 +1,8 @@
 module.exports = {
   url: process.env.URL || "http://localhost:8080",
-  siteName: "11ty Netlify Jumpstart",
+  siteName: "Welcome to SemiPIN",
   siteDescription:
-    "Quickly launch an 11ty-generated static site. Includes a minimal Sass framework, generated sitemap, and RSS feed.",
-  authorName: "Stephanie Eckles",
-  twitterUsername: "5t3ph", // no `@`
+    "Your instrumentation desgin partner.",
+  authorName: "SemiPIN",
+  twitterUsername: "", // no `@`
 };
